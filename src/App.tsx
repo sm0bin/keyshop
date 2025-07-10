704c1e7b4500c17e804eb49486019282ffbba5fd
sm0bin <s1810677110@ru.ac.bd>
2025-07-10 21:08:54 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
